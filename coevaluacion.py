6df1b8625dc739bb960f8a033471d338b33fd533
wilton <162214078+Wil2024@users.noreply.github.com>
2026-04-28 15:20:20 -0500

    Update coevaluacion.py

--- coevaluacion.py
+++ coevaluacion.py
@@ -8,7 +8,7 @@ CLAVE_DOCENTE = "docentejwts123"
 
 equipos_estudiantes = {
     "Selecciona tu equipo":[],
-    "Equipo 1": ["Selecciona tu nombre", "Deans Cabrera", "Miguel Herrera", "Eleana Navio", "Deisy Salazar", "Gianfranco Vaccari"],
+    "Equipo 1": [ "Deans Cabrera", "Miguel Herrera", "Eleana Navio", "Deisy Salazar", "Gianfranco Vaccari"],
     "Equipo 2": ["Daniel Pinedo", "Jorge Acero", "Milagro Molina", "Sergio Valencia", "Yoseff Vilcapoma"],
     "Equipo 3": ["Andrés Álvarez", "Jacklyn Beraún", "Oscar Garnique", "Rafael Marca", "Nohelia Tang", "Jessica Timana"]
 }
